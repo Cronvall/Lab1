36261d10144b3fbf0e656059d75918663156851b
Cronvall <47293083+Cronvall@users.noreply.github.com>
2023-11-01 14:54:39 +0100

    reconfigure

--- go.mod
+++ go.mod
@@ -1,8 +1,3 @@
 module mod
 
 go 1.21.3
-
-require (
-	github.com/google/uuid v1.4.0
-	github.com/gorilla/mux v1.8.0
-)
--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
+	"log"
 	"net/http"
-
-	"./api"
 )
 
-func main() {
-	srv := api.NewServer()
-	http.ListenAndServe(":8080", srv.Router)
+type Book struct {
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Pages  int    `json:"pages"`
+}
 
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	book := Book{
+		"Building Web Apps with Go", // Title
+		"Jeremy Saenz",              // Author
+		256,                         // Pages
+	}
+
+	json.NewEncoder(w).Encode(book)
+}
+
+func Hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.Write([]byte("<h1 style='color: steelblue'>Hello World!</h1>"))
+}
+
+func main() {
+	http.HandleFunc("/hello", Hello)
+	http.HandleFunc("/book", GetBook)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
